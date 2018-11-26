d04846da414d7f900147b7f45c12975b07cc36b0
Filip Kostovski <github.sirfilip@gmail.com>
2018-11-26 01:06:14 +0100

    Add csrf protection

--- web/create.go
+++ web/create.go
@@ -1,10 +1,12 @@
 package web
 
 import (
-	"log"
+	"errors"
 	"net/http"
+	"time"
 
 	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
 
 	"blogger/posts"
 )
@@ -13,16 +15,31 @@ type CreatePostForm struct {
 	Title  string `valid:"required~Title is blank"`
 	Body   string `valid:"required~Body is blank"`
 	Author string `valid:"required~Author is blank"`
-	Error  error  `valid:"-"`
+	err    error  `valid:"-"`
+	Csrf   string `valid:"-"`
 }
 
-func (self *CreatePostForm) Submit(r *http.Request) error {
+func (self *CreatePostForm) Error() error {
+	return self.err
+}
+
+func (self *CreatePostForm) Submit(r *http.Request) {
+	csrf, err := r.Cookie("csrf")
+	if err != nil {
+		self.err = errors.New("CSRF failed!")
+		return
+	}
+	if csrf == nil {
+		self.err = errors.New("CSRF failed!")
+	}
+	if csrf.Value != r.FormValue("csrf") {
+		self.err = errors.New("CSRF failed!")
+		return
+	}
 	self.Title = r.FormValue("title")
 	self.Body = r.FormValue("body")
 	self.Author = r.FormValue("author")
-	_, self.Error = govalidator.ValidateStruct(self)
-	log.Printf("Got error: %v", self.Error)
-	return self.Error
+	_, self.err = govalidator.ValidateStruct(self)
 }
 
 func (self *Web) Create(w http.ResponseWriter, r *http.Request) {
@@ -30,7 +47,7 @@ func (self *Web) Create(w http.ResponseWriter, r *http.Request) {
 	form := &CreatePostForm{}
 	ctx.Set("Form", form)
 	if r.Method == "POST" {
-		if err := form.Submit(r); err == nil {
+		if form.Submit(r); form.Error() == nil {
 			post, err := self.postRepo.Save(&posts.Post{
 				Title:  form.Title,
 				Body:   form.Body,
@@ -45,5 +62,12 @@ func (self *Web) Create(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
+	csrf, _ := uuid.NewV4()
+	form.Csrf = csrf.String()
+	http.SetCookie(w, &http.Cookie{
+		Name:    "csrf",
+		Value:   csrf.String(),
+		Expires: time.Now().Add(5 * time.Minute),
+	})
 	self.ExecuteTemplate(w, "create.html", ctx)
 }
